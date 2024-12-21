5a8853b5b7a3b11fe70e2f6616f92a91ac9b8f7b
Barışcan Çeken <144244486+bariscanceken@users.noreply.github.com>
2024-12-21 21:18:57 +0300

    Code Edited And Datas Saved.

--- main.py
+++ main.py
@@ -3,71 +3,38 @@ import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+import os
 
+#os
+base_dir = os.path.dirname(os.path.abspath(__file__))
+path_diarydata = os.path.join(base_dir, 'diarydata.txt')
 
-
+#selenium
 driver = webdriver.Chrome()
 driver.maximize_window()
-
 datas = []
-driver.get("https://letterboxd.com/aliblade/films/diary/")
+who = input("Enter Username: ")
+link = f"https://letterboxd.com/{who}/films/diary/"
+driver.get(link)
 data = driver.find_element(By.XPATH,"/html/body/div[1]/div/section[2]/table/tbody")
 datas.append(data.text)
+driver.get(link)
 
-'''
 try:
+    data = driver.find_element(By.XPATH,"/html/body/div[1]/div/section[2]/table/tbody")
+    datas.append(data.text)
     older_button = driver.find_element(By.CLASS_NAME,"next")
     WebDriverWait(driver,10).until(expected_conditions.element_to_be_clickable((By.XPATH,'/html/body/div[1]/div/section[2]/div[2]/div[3]/ul')))
     pages = driver.find_element(By.XPATH,'/html/body/div[1]/div/section[2]/div[2]/div[3]/ul')
-    for i in range(len(pages.text.splitlines())):
-        print("im here",len(pages.text.splitlines()))
-        WebDriverWait(driver,10).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME,"next")))
-        older_button.click()
-        time.sleep(2)
-        data = driver.find_element(By.XPATH,"/html/body/div[1]/div/section[2]/table/tbody") #/html/body/div[1]/div/section[2]/div[2]/div[3]/ul /html/body/div[1]/div/section[2]/table/tbody /html/body/div[1]/div/section[2]/div[2]/div[3]/ul /html/body/div[1]/div/section[2]/div[2]/div[3]/ul
-        datas.append(data.text)
-        print(datas)
-
-except:
-    pass
-
-'''
-
-try:
-    older_button = driver.find_element(By.CLASS_NAME,"next")
-    WebDriverWait(driver,10).until(expected_conditions.element_to_be_clickable((By.XPATH,'/html/body/div[1]/div/section[2]/div[2]/div[3]/ul')))
-    pages = driver.find_element(By.XPATH,'/html/body/div[1]/div/section[2]/div[2]/div[3]/ul')
-    for i in range(1,len(pages.text.splitlines())+ 1):
-        driver.get(f"https://letterboxd.com/aliblade/films/diary/page/{i}/")
+    with open(path_diarydata, 'a',encoding="utf-8") as dosya:
+        dosya.write(datas[0])
+    for i in range(2,len(pages.text.splitlines())+ 1):
+        driver.get(f"{link}page/{i}/")
         time.sleep(2)
         data = driver.find_element(By.XPATH,"/html/body/div[1]/div/section[2]/table/tbody")
         datas.append(data.text)
-    print(datas)
-
+        with open(path_diarydata, 'a',encoding="utf-8") as dosya:
+            dosya.write(datas[i])
 except:
-    pass
-
-
-
-
-
-
-
-
-'''
-while driver.find_element(By.CLASS_NAME,"next"):
-    older_button.click()
-    data = driver.find_element(By.XPATH,"/html/body/div[1]/div/section[2]/table/tbody")
-    datas.append(data.text)
-
-print(datas)
-
-
-findsnothing.send_keys("Barisc36.")
-findsnothing.send_keys(Keys.ENTER)
-#WebDriverWait(driver,10).until(expected_conditions.element_to_be_clickable((By.NAME,"btnK")))
-
-#search_button.click()
-'''
-time.sleep(30)
+    with open(path_diarydata, 'a',encoding="utf-8") as dosya:
+        dosya.write(datas[0])
